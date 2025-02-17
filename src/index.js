014911abb154d1c8aeebbd51df408228e9bf529a
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-02-17 04:51:19 -0500

    w3 Add forms

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// My code
+import MyForm from "./myDoc/MyForm";
+import MyFormMul from "./myDoc/MyFormMul";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MyForm />
+    <MyFormMul />
   </React.StrictMode>
 );
 
