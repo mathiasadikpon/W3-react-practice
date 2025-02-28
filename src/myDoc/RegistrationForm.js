3935a6a4bb9c4b08767b63325411bb8c97102ef6
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-02-28 18:57:50 -0500

    add RegistrationForm component with Formik integration

---
+++ src/myDoc/RegistrationForm.js
@@ -0,0 +1,13 @@
+import { Button, Label, Col, FormGroup } from "bootstrap";
+import { Formik,Field, Form, ErrorMessage } from "formik";
+
+const RegistrationForm = () => {
+
+    return <Formik>
+        <Form>
+            
+        </Form>
+    </Formik>
+};
+
+export default RegistrationForm;
